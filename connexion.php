c0b2bd8bb5ab723666dfd8e68178385136684cd6
Chanias <85565864+Chanias@users.noreply.github.com>
2022-03-03 15:28:57 +0100

    TEST

---
+++ connexion.php
@@ -0,0 +1,13 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+try
+{
+    $bdd = new PDO('mysql:host=127.0.0.1:8889;dbname=site_vitrine;charset=utf8', 'Floriane', 'Diablo18!!', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+?>
